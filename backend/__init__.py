df848595e1ba2c2ed974035cf4c94af4485e9b66
sallapuram yaswanth reddy <sallapuramyaswanthred123@gmail.com>
2026-08-26 07:45:39 +0000

    fnal fix - CI CD workflows and dockerfiles

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,12 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-
 from .movies import movies_api
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(movies_api)
 
-# Start app
 if __name__ == "__main__":
     app.run(
         debug=True,
